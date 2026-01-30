5aa847fc6ccd3f78064455276bacd5e2a193f234
Halffd <guhalfeld@hotmail.com>
2026-01-30 19:36:39 -0300

    build: enable all available plugins in build script

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ build() {
     -DCOMPIZ_PACKAGING_ENABLED=ON
     -DBUILD_GTK=ON
     -DBUILD_METACITY=ON
-    -DCOMPIZ_DEFAULT_PLUGINS="composite,opengl,decor,resize,place,move,compiztoolbox,staticswitcher,regex,animation,wall,ccp"
+    -DCOMPIZ_DEFAULT_PLUGINS="addhelper,annotate,animation,animationaddon,animationjc,animationplus,bench,bicubic,blur,ccp,clone,colorfilter,commands,compiztoolbox,composite,copytex,crashhandler,cube,cubeaddon,dbus,decor,expo,extrawm,ezoom,fade,fadedesktop,firepaint,freewins,gears,gnomecompat,grid,group,imgjpeg,imgpng,imgsvg,inotify,loginout,mag,matecompat,maximumize,mblur,mousepoll,move,neg,notification,obs,opacify,opengl,place,put,reflex,regex,resize,resizeinfo,ring,rotate,scale,scaleaddon,scalefilter,screenshot,session,shelf,shift,showdesktop,showmouse,showrepaint,simple-animations,snap,splash,stackswitch,staticswitcher,switcher,td,text,thumbnail,titleinfo,trailfocus,trip,vpswitch,wall,wallpaper,water,widget,winrules,wizard,wobbly,workarounds,workspacenames"
     -DCOMPIZ_BUILD_TESTING=OFF
     -DCOMPIZ_WERROR=OFF
     -Wno-dev
